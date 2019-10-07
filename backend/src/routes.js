8455154e2f12f9e970c9c24a567f44786c6826c2
Raphael Ornellas <raphaelso@live.com>
2019-10-06 22:28:33 -0300

    project finish

---
+++ backend/src/routes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const multer = require("multer");
+const uploadConfig = require("./config/upload");
+
+const routes = express.Router();
+const upload = multer(uploadConfig);
+
+const SessionController = require("./controllers/SessionController");
+const SpotController = require("./controllers/SpotController");
+const DashboardController = require("./controllers/DashboardController");
+const BookingController = require("./controllers/BookingController");
+const ApprovalController = require("./controllers/ApprovalController");
+const RejectionController = require("./controllers/RejectionController");
+
+routes.post("/sessions", SessionController.store);
+routes.get("/dashboard", DashboardController.show);
+routes.get("/spots", SpotController.index);
+routes.post("/spots", upload.single("thumbnail"), SpotController.store);
+routes.post("/spots/:spot_id/bookings", BookingController.store);
+routes.post("/bookings/:booking_id/approvals", ApprovalController.store);
+routes.post("/bookings/:booking_id/rejections", RejectionController.store);
+
+module.exports = routes;
---
+++ mobile/src/pages/Login.js
@@ -0,0 +1,119 @@
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  AsyncStorage,
+  KeyboardAvoidingView,
+  Platform,
+  Image,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  StyleSheet
+} from "react-native";
+
+import api from "../services/api";
+
+import logo from "../assets/logo.png";
+
+export default function Login({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [techs, setTechs] = useState("");
+
+  // useEffect(() => {
+  //   AsyncStorage.getItem("user").then(user => {
+  //     if (user) navigation.navigate("List");
+  //   });
+  // }, []);
+
+  const handleSubmit = async () => {
+    const response = await api.post("/sessions", {
+      email
+    });
+
+    const { _id } = response.data;
+
+    await AsyncStorage.setItem("user", _id);
+    await AsyncStorage.setItem("techs", techs);
+
+    navigation.navigate("List");
+  };
+
+  return (
+    <KeyboardAvoidingView
+      enabled={Platform.OS === "ios"}
+      behavior="padding"
+      style={styles.container}
+    >
+      <Image source={logo}></Image>
+
+      <View style={styles.form}>
+        <Text style={styles.label}>SEU E-MAIL *</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Seu e-mail"
+          placeholderTextColor="#999"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          autoCorrect={false}
+          onChangeText={text => setEmail(text)}
+          value={email}
+        />
+
+        <Text style={styles.label}>TECNOLOGIAS *</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Tecnologias de interesse"
+          placeholderTextColor="#999"
+          autoCapitalize="words"
+          autoCorrect={false}
+          onChangeText={text => setTechs(text)}
+          value={techs}
+        />
+
+        <TouchableOpacity onPress={handleSubmit} style={styles.button}>
+          <Text style={styles.buttonText}>Encontrar spots</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  label: {
+    fontWeight: "bold",
+    color: "#444",
+    marginBottom: 8
+  },
+  form: {
+    alignSelf: "stretch",
+    paddingHorizontal: 30,
+    marginTop: 30
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ddd",
+    paddingHorizontal: 20,
+    fontSize: 16,
+    color: "#444",
+    height: 44,
+    marginBottom: 20,
+    borderRadius: 2
+  },
+  button: {
+    height: 42,
+    backgroundColor: "#f05a5b",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 2
+  },
+  buttonText: {
+    color: "#FFF",
+    fontWeight: "bold",
+    fontSize: 16
+  }
+});
